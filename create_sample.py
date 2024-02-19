a11b094261b5093ecb0400e0b632a077d97a18d0
jonatank <jonatan.kronander@inivation.com>
2024-02-19 19:15:47 +0100

    adding city input

--- create_sample.py
+++ create_sample.py
@@ -2,14 +2,17 @@ import argparse
 import os
 from quiz import quiz_creator, street_view_collector, video_creator
 
-
+'''
+Create a mobile version sample. Used to create a sample quiz and video for the mobile version of the quiz.
+'''
 def main():
     parser = argparse.ArgumentParser(description='Create a new quiz.')
     parser.add_argument('out', type=str, help='The folder to use for the quiz.')
+    parser.add_argument('city', type=str, help='What destination to use for the quiz.')
 
     args = parser.parse_args()
 
-    quiz_creator.create_new_quiz("./data")
+    quiz_creator.create_new_quiz("./data", args.city)
     street_view_collector.create_new_frames("./data", "mobile")
     video_creator.create_new_video("./data", args.out)
 
